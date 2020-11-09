e8b03c86be0a04cf928e72403febd072d761742d
tranhuuduc215 <tranhuuduc215@gmai.com>
2020-11-09 17:58:10 +0700

    Add project files.

---
+++ WebDemo/Controller/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDemo.Models;
+
+namespace WebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository employeeRepository;
+        public HomeController()
+        {
+            employeeRepository = new EmployeeRipository();
+        }
+
+        public ViewResult Index()
+        {
+            //viewdata
+            ViewData["employee"] = employeeRepository.Gets();
+            return View();
+        }
+    }
+}
---
+++ WebDemo/Models/EmployeeRipository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemo.Models
+{
+    public class EmployeeRipository : IEmployeeRepository
+    {
+        private List<Employee> employees;
+        public EmployeeRipository()
+        {
+            employees = new List<Employee>
+            {
+                new Employee(){Id = 1, Fullname = "Trung Quốc", Department = "IT", Email="trungquoc@gmail.com", AvatarPath="~/images/xuongrong1.jpg"},
+                new Employee(){Id = 2, Fullname = "Trung Cộng", Department = "HR", Email="trungcong@gmail.com", AvatarPath="~/images/xuongrong1.jpg"},
+                new Employee(){Id = 3, Fullname = "Ái Quốc", Department = "Sale", Email="aiquoc@gmail.com", AvatarPath="~/images/xuongrong1.jpg"},
+                new Employee(){Id = 4, Fullname = "Tổ Quốc", Department = "GV", Email="toquoc@gmail.com", AvatarPath="~/images/xuongrong1.jpg"},
+                new Employee(){Id = 5, Fullname = "Quốc Tế", Department = "Dep", Email="quocte@gmail.com", AvatarPath="~/images/xuongrong1.jpg"}
+
+            };
+        }
+        public IEnumerable<Employee> Gets()
+        {
+            return employees;
+        }
+    }
+}
